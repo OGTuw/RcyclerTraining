cf837b8555718faa0a2c76e5ff8f6c01fe9f06d6
ogatatatsuya <uw.roxas@icloud.com>
2020-04-17 23:30:37 +0900

    20200416

---
+++ app/src/main/java/com/example/rcyclertraining/actress_items/FragmentMatsushita.java
@@ -0,0 +1,44 @@
+package com.example.rcyclertraining.actress_items;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.rcyclertraining.R;
+
+public class FragmentMatsushita extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // R.layout.Fragment(xml)のファイル名
+        return inflater.inflate(R.layout.fragment_matsushita, null);
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // FragmentにあるImageViewを取得。
+        // FragmentクラスではR.Id.〜は使えないので、view.〜って書く。
+        ImageView img = view.findViewById(R.id.image_picture);
+        // 画像をいい感じにしてくれるGlide様。
+        // withの引数は確定でgetContext()。
+        // loadの引数は乗っけたい画像をR(res).drawable.画像名
+        Glide.with(getContext()).load(R.drawable.matsushita).into(img);
+    }
+}
